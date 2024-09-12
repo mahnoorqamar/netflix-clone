2c59f4958e311a8e5df9143b6b753399b4a5ab0e
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-09-13 00:58:56 +0530

    tv routes

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { getTrendingMovie } from "../controllers/movie.controller.js";
+import {
+  getMovieDetails,
+  getMovieTrailers, getTrendingMovie, getSimilarMovies,
+  getMoviesByCategory,
+} from "../controllers/movie.controller.js";
+
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
+router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovies);
+router.get("/category", getMoviesByCategory);
 
 export default router;
