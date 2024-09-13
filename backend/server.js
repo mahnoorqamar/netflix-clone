1fb793399221b5f57900b4b4c6caeba60d6d29d2
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-09-13 23:05:26 +0530

    searchRouter

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import tvRoutes from "./routes/tv.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import cookieParser from "cookie-parser";
+
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
@@ -14,10 +17,12 @@ app.get("/", (req, res) => {
 });
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 // Connect to DB first, then start server
 connectDB().then(() => {
